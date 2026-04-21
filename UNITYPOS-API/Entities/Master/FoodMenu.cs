3186ca5b815537b888a80b08ca956ce5b86b50c7
POS2 <rajeshkumar16rko@gmail.com>
2026-04-21 09:16:38 +0530

    Update Entities

--- UNITYPOS-API/Entities/Master/FoodMenu.cs
+++ UNITYPOS-API/Entities/Master/FoodMenu.cs
@@ -2,6 +2,7 @@
 {
     public class FoodMenu : CommonClass
     {
+        public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
--- UNITYPOS-API/Entities/Master/FoodMenuSubCategory.cs
+++ UNITYPOS-API/Entities/Master/FoodMenuSubCategory.cs
@@ -2,6 +2,7 @@
 {
     public class FoodMenuSubCategory : CommonClass
     {
+        public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public int CategoryId { get; set; }
