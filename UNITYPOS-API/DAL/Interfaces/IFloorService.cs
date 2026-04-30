538a1f314de7e7c5d1f8da75080a9b3514def849
User <vjvimala123@gmail.com>
2026-04-30 12:59:06 +0530

    floor1

---
+++ UNITYPOS-API/DAL/Interfaces/IFloorService.cs
@@ -0,0 +1,14 @@
+﻿using UNITYPOS_API.Entities.Master;
+
+namespace UNITYPOS_API.DAL.Interfaces
+{
+    public interface IFloorService
+    {
+        public string Create(FloorMaster floor);
+        public string Update(FloorMaster floor);
+        public IEnumerable<Object> GetAllFloor(int orgid, int branchid);
+        public FloorMaster GetById(int Id);
+        public string Delete(int Id);
+        public string ActiveInActive(int Id, bool IsActive);
+    }
+}
