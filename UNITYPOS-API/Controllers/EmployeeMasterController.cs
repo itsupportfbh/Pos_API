5fdaf6358a50ec9d6adc57694ec0637fe87b862f
Mohan9620-T <mohanthangaraj9620@gmail.com>
2026-05-20 15:34:01 +0800

    employee master changes

---
+++ UNITYPOS-API/Controllers/EmployeeMasterController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UNITYPOS_API.DAL.Interfaces;
+using UNITYPOS_API.Entities.Master;
+using UNITYPOS_API.Entities;
+
+namespace UNITYPOS_API.Controllers
+
+{
+    
+        [Route("[controller]/[action]")]
+        [ApiController]
+        [Authorize]
+        public class EmployeeController : ControllerBase
+        {
+            private readonly IEmployeeMasterService _employeeMasterService;
+
+            public EmployeeController(IEmployeeMasterService employeeService)
+            {
+                _employeeMasterService = employeeService;
+            }
+
+            [HttpGet]
+            public string GetAllEmployee(int orgId, string branchId)
+            {
+                string result = null;
+
+                result = JsonConvert.SerializeObject(
+                    _employeeMasterService.GetAllEmployee(orgId, branchId));
+
+                return Common.Utility.GetResult(result);
+            }
+
+            [HttpGet]
+            public string GetEmployeebyId(int id)
+            {
+                string result = null;
+
+                result = JsonConvert.SerializeObject(
+                    _employeeMasterService.GetEmployeebyId(id));
+
+                return Common.Utility.GetResult(result);
+            }
+
+        [HttpPost]
+        public string Create(EmployeeMaster request)
+        {
+            string result = JsonConvert.SerializeObject(
+                _employeeMasterService.Create(request));
+
+            return Common.Utility.GetResult(result);
+        }
+
+
+
+        [HttpPut]
+        public string Update(EmployeeMaster request)
+        {
+
+            string result = JsonConvert.SerializeObject(
+                _employeeMasterService.Update(request));
+
+            return Common.Utility.GetResult(result);
+        }
+
+        [HttpDelete]
+            public string Delete(int id)
+            {
+                string result = null;
+
+                result = JsonConvert.SerializeObject(
+                    _employeeMasterService.DeleteById(id));
+
+                return Common.Utility.GetResult(result);
+            }
+
+            [HttpPut]
+            public string ActiveInActive(int Id, bool IsActive)
+            {
+                string result = null;
+
+                result = JsonConvert.SerializeObject(
+                    _employeeMasterService.ActiveInActive(Id, IsActive));
+
+                return Common.Utility.GetResult(result);
+            }
+        }
+    }
+
---
+++ UNITYPOS-API/DAL/Interfaces/IEmployeeMasterService.cs
@@ -0,0 +1,18 @@
+﻿using UNITYPOS_API.Controllers;
+using UNITYPOS_API.Entities.Master;
+using UNITYPOS_API.Entities;
+
+
+namespace UNITYPOS_API.DAL.Interfaces
+{
+    public interface IEmployeeMasterService
+    {
+
+        public IEnumerable<object> GetAllEmployee(int orgId, string branchId);
+        public EmployeeMaster GetEmployeebyId(int Id);
+        public string DeleteById(int id);
+        public string ActiveInActive(int id, bool isActive);
+        public string Create(EmployeeMaster employeeMaster);
+        string Update(EmployeeMaster request);
+    }
+}
