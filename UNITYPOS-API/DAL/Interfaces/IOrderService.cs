c8f3c7c21e59f6ce24f608835379458fda0a7a3d
User <vjvimala123@gmail.com>
2026-05-27 10:06:32 +0530

    order

--- UNITYPOS-API/DAL/Interfaces/IOrderService.cs
+++ UNITYPOS-API/DAL/Interfaces/IOrderService.cs
@@ -5,13 +5,9 @@ namespace UNITYPOS_API.DAL.Interfaces
     public interface IOrderService
     {
         
-        IEnumerable<object> GetAllOrderDetails(long orderId, long branchId);
-        Orders? GetById(long orderId);
-        string Delete(long orderId);
-
-        public string GenerateOrderNumber(int orgId, int branchId);
-
-
+        IEnumerable<object> GetAllOrderDetails(int orderId, int branchId);
+        Orders? GetById(int orderId);
+        string Delete(int orderId);
         public Task<string> Create(Orders orders);
         public Task<string> Update(Orders orders);
         public string StatusChange(Orders order);
